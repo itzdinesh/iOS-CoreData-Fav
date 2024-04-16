4ae66818998126b0cb538702338755a024102da7
cynotecck <bipin.rawat@cynoteck.com>
2024-04-16 11:23:50 +0530

    Initial Commit

---
+++ TestingProject/TestingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingProjectApp.swift
+//  TestingProject
+//
+//  Created by Cynotecck on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
